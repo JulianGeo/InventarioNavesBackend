c1f77015b6ae89cde0f8a9f4bfe049b71f01f121
Julian Rojas <julianrojasgeo@gmail.com>
2022-07-14 17:19:07 -0500

    first commit

---
+++ src/main/java/com/example/SofkaNaves/SofkaNavesApplication.java
@@ -0,0 +1,13 @@
+package com.example.SofkaNaves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofkaNavesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofkaNavesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SofkaNaves/SofkaNavesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SofkaNaves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofkaNavesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
